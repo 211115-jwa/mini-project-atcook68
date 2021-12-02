2abcda8a4265599f7f2ffd3dae9413c7b0bbcbbe
Andrew Cook <56707541+atcook68@users.noreply.github.com>
2021-12-02 11:03:30 -0800

    Update Collection.java

--- src/main/java/com/revature/Collection.java
+++ src/main/java/com/revature/Collection.java
@@ -6,6 +6,10 @@ public class Collection {
 	public int age;
 	public String breed;
 	
+	
+	// Creating the Collection class for the favorite pets
+	
+	
 	@Override
 	public String toString() {
 		return name;
